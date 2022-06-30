1ccf12a2f5993aea2e7ded1f73f3bbdeb147b0f0
Yagna603 <84460990+Yagna603@users.noreply.github.com>
2022-07-01 00:42:57 +0530

    Update BigInteger_Lib.h

--- BigInteger_Lib.h
+++ BigInteger_Lib.h
@@ -2,22 +2,22 @@
 #define MAX 10000 // for strings
 
 using namespace std;
-//-------------------------------------------------------------
+
 class BigInteger
 {
 private:
 	string number;
 	bool sign;
 public:
-	BigInteger(); // empty constructor initializes zero
-	BigInteger(string s); // "string" constructor
-	BigInteger(string s, bool sin); // "string" constructor
-	BigInteger(int n); // "int" constructor
+	BigInteger();		 // empty constructor initializes zero
+	BigInteger(string s); 		// "string" constructor
+	BigInteger(string s, bool sin); 	// "string" constructor
+	BigInteger(int n); 		// "int" constructor
 	void setNumber(string s);
-	const string& getNumber(); // retrieves the number
+	const string& getNumber();	// retrieves the number
 	void setSign(bool s);
 	const bool& getSign();
-	BigInteger absolute(); // returns the absolute value
+	BigInteger absolute(); 		// returns the absolute value
 	void operator = (BigInteger b);
 	bool operator == (BigInteger b);
 	bool operator != (BigInteger b);
@@ -25,10 +25,10 @@ public:
 	bool operator < (BigInteger b);
 	bool operator >= (BigInteger b);
 	bool operator <= (BigInteger b);
-	BigInteger& operator ++(); // prefix
-	BigInteger  operator ++(int); // postfix
-	BigInteger& operator --(); // prefix
-	BigInteger  operator --(int); // postfix
+	BigInteger& operator ++(); 	// prefix
+	BigInteger  operator ++(int); 		// postfix
+	BigInteger& operator --();	// prefix
+	BigInteger  operator --(int); 		// postfix
 	BigInteger operator + (BigInteger b);
 	BigInteger operator - (BigInteger b);
 	BigInteger operator * (BigInteger b);
@@ -40,8 +40,8 @@ public:
 	BigInteger& operator /= (BigInteger b);
 	BigInteger& operator %= (BigInteger b);
 	BigInteger& operator [] (int n);
-	BigInteger operator -(); // unary minus sign
-	operator string(); // for conversion from BigInteger to string
+	BigInteger operator -(); 		// unary minus sign
+	operator string(); 		// for conversion from BigInteger to string
 private:
 	bool equals(BigInteger n1, BigInteger n2);
 	bool less(BigInteger n1, BigInteger n2);
